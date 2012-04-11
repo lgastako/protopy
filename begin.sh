3a536a355df48c19652575fad78d192618577857
John Evans <lgastako@gmail.com>
2012-04-11 01:06:57 -0700

    Basically working.

--- begin.sh
+++ begin.sh
@@ -7,4 +7,22 @@ fi
 
 PROJECT_NAME="$1"
 
+echo "Autotransmogrifying into: ${PROJECT_NAME}"
+
+. /usr/local/bin/virtualenvwrapper.sh
+
+# We `` to get the mkvirtualenv stuff out of the bash env
+mkvirtualenv ${PROJECT_NAME}
+
+pip install -r requirements.txt
+
 perl -pi -e "s/{{NAME}}/${PROJECT_NAME}/" setup.py
+
+pip install -e .
+
+easy_install readline
+
+rm -rf .git
+
+git init
+
