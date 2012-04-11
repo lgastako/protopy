cec98f2a2d21c2aa9dd1d42a60bdce60b7c3fdb4
John Evans <lgastako@gmail.com>
2012-04-11 00:51:51 -0700

    More mustaches are always a good time.

--- begin.sh
+++ begin.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ X"$0" = X ]; then
+if [ "$*" = "" ]; then
     echo "You must specify exactly one name on the command line."
     exit
 fi
 
 name=$0
 
-perl -pi -e "s/.../${name}" setup.py
+perl -pi -e "s/{{NAME}}/$name" setup.py
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name="...",
+setup(name="{{NAME}}",
       version="0.0.1",
       description="Python Distribution Utilities",
       author="John Evans",
       author_email="lgastako@gmail.com",
-      url="https://github.com/lgastako/...",
+      url="https://github.com/lgastako/{{NAME}}",
       package_dir={"":"lib"},
       scripts=[]
 )
